a8d91b86c90c8aa87066e333b9ce56515ae588c0
Sebastian Cepeda <juanydani94@gmail.com>
2023-02-09 18:45:35 -0500

    añadiendo pruebas 5 casos equivalencia

--- ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
+++ ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -1,16 +1,28 @@
 package edu.eci.cvds.tdd.registry;
-
+import java.util.ArrayList;
 public class Registry {
+    private ArrayList<Integer> idPerson;
+
+    public Registry (){
+        idPerson = new ArrayList<Integer>();
+    }
     public RegisterResult registerVoter(Person p) {
-        if(p.getAge() <= 0){
+        int idP = p.getId();
+        if(idPerson.contains(idP)){
+            return RegisterResult.DUPLICATED;
+        }
+        else if(p.getAge() <= 0){
             return RegisterResult.INVALID_AGE;
         }
-        else if(p.isAlive()){
+        else if(!p.isAlive()){
             return RegisterResult.DEAD;
         }
         else if(p.getAge()<= 17 &&  p.getAge()>0){
             return RegisterResult.UNDERAGE;
         }
-        return RegisterResult.VALID;
+        else{
+            idPerson.add(idP);
+            return RegisterResult.VALID;
+        }
     }
 }
--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -7,14 +7,18 @@ public class RegistryTest {
     private Registry registry = new Registry();
     @Test
     public void validateRegistryResult() {
-        Person person = new Person();
+        Gender gender = Gender.FEMALE;
+        Person person = new Person("andrea",  121, 45, gender, true);
         RegisterResult result = registry.registerVoter(person);
         Assert.assertEquals(RegisterResult.VALID, result);  
     }
 
     @Test
     public void invalIdAGE (){
-        
+        Gender gender = Gender.FEMALE;
+        Person person = new Person("SOFIA",  12, -1, gender, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.INVALID_AGE, result);  
     }
 
 }
